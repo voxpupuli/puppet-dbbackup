7fe2a83946000a7127b264f7755959f6a50a7f9f
Tim Meusel <tim@bastelfreak.de>
2020-10-19 13:48:22 +0200

    fix puppet-lint warnings

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,10 @@
 class dbbackup (
-  Integer $pigz_cpu_cores             = $facts['processorcount'],
+  Integer $pigz_cpu_cores             = $facts['processors']['count'],
   Stdlib::Absolutepath $destination   = '/mnt/dumps',
   String $interval                    = '1h',
   Optional[Integer[1]] $backuphistory = undef,
   Boolean $cleanup_empty_backup_dirs = true,
-)
-
-{
+) {
   contain dbbackup::install
   contain dbbackup::service
 
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,16 +1,16 @@
 class dbbackup::service {
   assert_private("You're calling a private class, you're not supposed to do this")
-  systemd::unit_file{'dump_databases.service':
+  systemd::unit_file { 'dump_databases.service':
     content => epp("${module_name}/dump_databases.service.epp", {
-      'backuphistory'             => $dbbackup::backuphistory,
-      'destination'               => $dbbackup::destination,
-      'cleanup_empty_backup_dirs' => $dbbackup::cleanup_empty_backup_dirs
-      }),
+        'backuphistory'             => $dbbackup::backuphistory,
+        'destination'               => $dbbackup::destination,
+        'cleanup_empty_backup_dirs' => $dbbackup::cleanup_empty_backup_dirs
+    }),
   }
-  -> systemd::unit_file{'dump_databases.timer':
-    content => epp("${module_name}/dump_databases.timer.epp", {'interval' => $dbbackup::interval}),
+  -> systemd::unit_file { 'dump_databases.timer':
+    content => epp("${module_name}/dump_databases.timer.epp", { 'interval' => $dbbackup::interval }),
   }
-  ~> service{'dump_databases.timer':
+  ~> service { 'dump_databases.timer':
     ensure => 'running',
     enable => true,
   }
