fe983e3600a803bc0c209ed67e9f704d8de297fe
Tim Meusel <tim@bastelfreak.de>
2020-10-19 20:41:10 +0200

    Enable acceptance tests

---
+++ spec/acceptance/init_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper_acceptance'
+
+describe 'dbbackup' do
+  context 'with a backup server' do
+    let(:pp) do
+      'include dbbackup'
+    end
+
+    it 'works idempotently with no errors' do
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe service('dump_databases.timer') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+    describe file('/usr/local/bin/dump_databases') do
+      it { is_expected.to be_executable }
+    end
+  end
+end
