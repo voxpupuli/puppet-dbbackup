50605866d9e6916b67c7da10e60819194c41adc1
Tim Meusel <tim@bastelfreak.de>
2020-10-19 14:51:08 +0200

    add puppet-strings documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,23 @@
+# @summary module to configure a systemd timer + script to backup postgres, mysql and ldap databases
+#
+# @param pigz_cpu_cores
+#   number of cpu cores used for backup compression
+# @param destination
+#   directory where backups will be stored (with one subdirectory per backup day)
+# @param interval
+#   how often the systemd timer will trigger the dump script. This will be used for the OnUnitInactiveSec option in the timer
+# @param backuphistory
+#   number of days to keep the backup history
+# @param cleanup_empty_backup_dirs
+#   enable/disable a post command to cleanup empty directories with $destination. They can occour if the system run out of free diskspace
+#
+# @author Tim Meusel <tim@bastelfreak.de>
 class dbbackup (
-  Integer $pigz_cpu_cores             = $facts['processors']['count'],
+  Integer[1] $pigz_cpu_cores          = $facts['processors']['count'],
   Stdlib::Absolutepath $destination   = '/mnt/dumps',
-  String $interval                    = '1h',
+  String[2] $interval                 = '1h',
   Optional[Integer[1]] $backuphistory = undef,
-  Boolean $cleanup_empty_backup_dirs = true,
+  Boolean $cleanup_empty_backup_dirs  = true,
 ) {
   file { '/usr/local/bin/dump_databases':
     content => epp("${module_name}/dump_databases.epp", {
