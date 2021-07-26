9659e81b5cb743ae72e62aa559e05d61acbb1160
Tim Meusel <tim@bastelfreak.de>
2021-07-26 16:51:17 +0200

    Add parameter `dependencies` to manage additional dependencies

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,10 @@
 #   number of days to keep the backup history
 # @param cleanup_empty_backup_dirs
 #   enable/disable a post command to cleanup empty directories with $destination. They can occour if the system run out of free diskspace
+# @param dependencies
+#   array of dependencies for the database dump script
+# @param manage_dependencies
+#   Boolean to enable/disable the installation of the dependencies. defaults to false because the parameter was added after the initial release
 #
 # @author Tim Meusel <tim@bastelfreak.de>
 class dbbackup (
@@ -18,6 +22,8 @@ class dbbackup (
   String[2] $interval                  = '1h',
   Optional[Integer[1]] $backuphistory  = undef,
   Boolean $cleanup_empty_backup_dirs   = true,
+  Array[String[1]] $dependencies       = ['pigz'],
+  Boolean $manage_dependencies         = false,
 ) {
   file { '/usr/local/bin/dump_databases':
     content => epp("${module_name}/dump_databases.epp", {
@@ -39,5 +45,14 @@ class dbbackup (
         'destination'               => $destination,
         'cleanup_empty_backup_dirs' => $cleanup_empty_backup_dirs,
     }),
+  }
+
+  if $manage_dependencies {
+    $dependencies.each |String[1] $package| {
+      package { $package:
+        ensure => 'installed',
+        before => Systemd::Timer['dump_databases.timer'],
+      }
+    }
   }
 }
